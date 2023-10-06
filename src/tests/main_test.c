6fdc78521cefcba23424cd0e3a04460751b1b09f
erosrolf <erosrolf@gmail.com>
2023-10-06 18:40:46 +0300

    add tests

---
+++ src/tests/main_test.c
@@ -0,0 +1,15 @@
+#include "tests.h"
+
+void run_test(Suite *s, int *fail) {
+  SRunner *runner = srunner_create(s);
+  srunner_run_all(runner, CK_NORMAL);
+  *fail = srunner_ntests_failed(runner);
+  srunner_free(runner);
+}
+
+int main() {
+  int num_of_fail = 0;
+  run_test(test_parser(), &num_of_fail);
+
+  return num_of_fail == 0 ? 0 : 1;
+}
