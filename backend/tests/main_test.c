c3eb81f2d3f603613191723c1315c6f3945f7715
erosrolf <erosrolf@gmail.com>
2023-10-16 18:44:48 +0300

    added some tests

--- backend/tests/main_test.c
+++ backend/tests/main_test.c
@@ -11,7 +11,7 @@ int main() {
   int num_of_fail = 0;
   run_test(test_token_parsing(), &num_of_fail);
   run_test(test_math_operation(), &num_of_fail);
-  // run_test(test_str_calc(), &num_of_fail);
+  run_test(test_str_calc(), &num_of_fail);
 
   return num_of_fail == 0 ? 0 : 1;
 }
--- backend/tests/test_str_calculate.c
+++ backend/tests/test_str_calculate.c
@@ -1,33 +1,48 @@
 #include "tests.h"
 
-// START_TEST(str_calc_test_01) {
-//   char str[10];
-//   sprintf(str, "2+2*2");
-//   double res = 0;
-//   int return_value = str_calc(str, &res);
-//   printf("return_value = %d\n", return_value);
-//   ck_assert_double_eq_tol(res, 6, 0.7);
-// }
-// END_TEST
-
-// START_TEST(str_calc_test_02) {
-//   char str[15];
-//   sprintf(str, "5/2-2.6*10");
-//   double res = 0;
-//   int return_value = str_calc(str, &res);
-//   printf("return_value = %d\n", return_value);
-//   ck_assert_double_eq_tol(res, -23.5, 0.7);
-// }
-// END_TEST
-
-START_TEST(str_calc_test_03) {
-  char str[15];
-  sprintf(str, "-5*3");
-  printf("str = %s\n", str);
+START_TEST(str_calc_test_01) {
+  char str[10];
+  sprintf(str, "2+2*2");
   double res = 0;
   int return_value = str_calc(str, &res);
-  printf("return_value = %d\n", return_value);
+  ck_assert_double_eq_tol(res, 6, 0.7);
+  ck_assert_int_eq(return_value, OK);
+}
+END_TEST
+
+START_TEST(str_calc_test_02) {
+  char str[] = "5/2-2.6*10";
+  double res = 0;
+  int return_value = str_calc(str, &res);
+  ck_assert_double_eq_tol(res, -23.5, 0.7);
+  ck_assert_int_eq(return_value, OK);
+}
+END_TEST
+
+START_TEST(str_calc_test_03) {
+  char str[] = "-5*3";
+  double res = 0;
+  int return_value = str_calc(str, &res);
   ck_assert_double_eq_tol(res, -15, 0.7);
+  ck_assert_int_eq(return_value, OK);
+}
+END_TEST
+
+// START_TEST(str_calc_test_04) {
+//   char str[] = "-(2+2)";
+//   double res = 0;
+//   int return_value = str_calc(str, &res);
+//   ck_assert_double_eq_tol(res, -4, 0.7);
+//   ck_assert_int_eq(return_value, OK);
+// }
+// END_TEST
+
+START_TEST(str_calc_test_05) {
+  char str[] = "3+(7-5/(2+3))*(7-2)";
+  double res = 0;
+  int return_value = str_calc(str, &res);
+  ck_assert_double_eq_tol(res, 33, 0.7);
+  ck_assert_int_eq(return_value, OK);
 }
 END_TEST
 
@@ -35,9 +50,11 @@ Suite *test_str_calc() {
   Suite *suite = suite_create("str_calc_suite");
 
   TCase *tc_str_calc = tcase_create("str_calc_test");
-  // tcase_add_test(tc_str_calc, str_calc_test_01);
-  // tcase_add_test(tc_str_calc, str_calc_test_02);
+  tcase_add_test(tc_str_calc, str_calc_test_01);
+  tcase_add_test(tc_str_calc, str_calc_test_02);
   tcase_add_test(tc_str_calc, str_calc_test_03);
+  // tcase_add_test(tc_str_calc, str_calc_test_04);
+  tcase_add_test(tc_str_calc, str_calc_test_05);
   suite_add_tcase(suite, tc_str_calc);
 
   return suite;
